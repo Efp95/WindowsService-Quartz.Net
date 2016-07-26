fadc70377e552ed6afdfdd382de21c21537d7d7a
Efp95 <edsonflorespalma@gmail.com>
2016-07-26 15:32:24 -0500

    Avoid Quartz.Net tracing unnecessary events

--- QuartzSolution/QuartzService/Program.cs
+++ QuartzSolution/QuartzService/Program.cs
@@ -1,23 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace QuartzService
+﻿namespace QuartzService
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         static void Main()
         {
-            var scheduler = new Scheduler.JobScheduler();
 #if DEBUG
+            // Run as Console Application
+            var scheduler = new Scheduler.JobScheduler();
             scheduler.Run();
 #else
+            // Run as Windows Service
             ServiceBase.Run(new MainService());
 #endif
         }
