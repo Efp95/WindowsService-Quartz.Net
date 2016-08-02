030c0f51b801fd032b84bbebab60bef5d1fce3e9
Efp95 <edsonflorespalma@gmail.com>
2016-08-01 19:13:40 -0500

    Use Topshelf to host as a service

--- QuartzSolution/QuartzService/Program.cs
+++ QuartzSolution/QuartzService/Program.cs
@@ -1,6 +1,7 @@
 ﻿using QuartzService.Container;
 using QuartzService.Scheduler;
 using System.ServiceProcess;
+using Topshelf;
 
 namespace QuartzService
 {
@@ -10,13 +11,37 @@ namespace QuartzService
         {
             NinjectConfig.Initialize();
 
+
 #if DEBUG
             // Run as Console Application
             var scheduler = new JobScheduler();
             scheduler.Run();
 #else
             // Run as Windows Service
-            ServiceBase.Run(new MainService());
+            HostFactory.Run(h =>
+            {
+                h.Service<JobScheduler>(s =>
+                {
+                    s.ConstructUsing(x => new JobScheduler());
+
+                    s.WhenStarted(ws => ws.Run());
+                    s.WhenStopped(ws => ws.Stop());
+
+                    s.WhenPaused(ws => ws.Stop());
+                    s.WhenContinued(ws => ws.Run());
+
+                    s.WhenShutdown(ws => ws.Stop());
+                });
+
+                h.RunAsLocalSystem();
+
+                h.SetDescription("Hosting from Topshelf");
+                h.SetDisplayName("My Quartz.Net Service");
+                h.SetServiceName("Quartz Scheduler Service");
+            });
+
+            // Requires sends 'install' command from CMD to appear in the services window
+            // i.e. [QuartzService.exe install]
 #endif
         }
     }
